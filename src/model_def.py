664982855d2628ceb9ea380918ef2cf747ce369d
Ruiqi <ruiqi.zhong@gatech.edu>
2018-05-01 17:20:59 -0400

    word level, hashtag excluded, macro-f=62.16

--- src/model_def.py
+++ src/model_def.py
@@ -1,32 +1,39 @@
 from keras.layers import Input, Dense, Conv1D, Embedding, concatenate, \
     GlobalMaxPooling1D, Dropout
 from keras.models import Model
+import pandas as pd
+
+def print_results_from_np(result_np):
+    result_np = result_np.T
+    d = [{'precision': r[0], 'recall': r[1], 'f-score': r[2], 'support': r[3]} for r in result_np]
+    df = pd.DataFrame(d)
+    print(df)
 
 # returns two tensors
 # one for input_content, the other for tensor before final classification
 def content2rep(option, vocab_size, max_len, drop_out=0.5,
-                filter=200, dense_size=256, embed_dim=300):
+                filter=200, dense_size=256, embed_dim=300,
+                kernel_range=(1,6)):
     input_content = Input(shape=(max_len,),
                           name= option + '_content_input')
     embed_layer = Embedding(vocab_size, embed_dim, input_length=max_len,
                             name= option + '_embed')
     e_i = embed_layer(input_content)
+    embed_drop_out = Dropout(drop_out, name=option + '_embed_dropout')
+    e_i = embed_drop_out(e_i)
 
-    """TODO Confirm that there is no dropout/activation here"""
     # convolutional layers
     conv_out = []
-    for kernel_size in range(1, 6):
+    for kernel_size in kernel_range:
         c = Conv1D(filter, kernel_size, activation='relu',
                    name= option + '_conv_' + str(kernel_size))(e_i)
         c = GlobalMaxPooling1D(name= option + '_max_pooling_' + str(kernel_size))(c)
-        """TODO confirm that the dropout comes after max pooling"""
         c = Dropout(drop_out, name= option + '_drop_out_' + str(kernel_size))(c)
         conv_out.append(c)
     agg = concatenate(conv_out)
 
     dense_layer = Dense(dense_size, activation='relu',
                         name= option + '_last')
-    """TODO confirm that there is no dropout here"""
     content_rep = dense_layer(agg)
 
     return input_content, content_rep
@@ -36,19 +43,46 @@ class NN_architecture:
     def __init__(self,
                  options,
                  word_vocab_size=30000, word_max_len=50,
-                 char_vocab_size=1000, char_max_len=150,
+                 char_vocab_size=1000, char_max_len=140,
                  drop_out=0.5,
-                 filter=200, dense_size=256, embed_dim=300,
+                 filter=200, dense_size=256, embed_dim=300, kernel_range=range(1,6),
                  pretrained_weight_dir=None, weight_in_keras=None,
                  context_dim=None, context_dense_size=None,
                  splex_dense_size=None):
+        """
+        Initilizing a neural network architecture according to the specification
+
+        Parameters
+        ----------
+        options: an array containing all the options considered in the neural network model ['char', 'word', 'context']
+                    (probably splex in the future)
+                    for each option, the input is mapped to a lower dimension,
+                    then the lower dimension representation of each option is concatenated
+                    and is followed by the final classification layer
+        word_vocab_size: number of word level vocabs to be considered
+        word_max_len: number of words in a tweet sentence
+        char_vocab_size: number of char level vocabs to be considered
+        char_max_len: number of chars in a tweet sentence
+        drop_out: dropout rate for regularization
+        filter: number of filters for each kernel size
+        dense_size: the size of the dense layer following the max pooling layer
+        embed_dim: embedding dimension for character and word level
+        kernel_range: range of kernel sizes
+        pretrained_weight_dir: a dictionary containing the pretrained weight.
+                    e.g. {'char': '../weights/char_ds.weights'} means that the pretrained weight for character level model
+                    is in ../weights/char_ds.weights
+        weight_in_keras: whether the weight is in Keras
+        context_dim: the dimension of context representation
+        context_dense_size: the dense layer size right before the context representation
+        splex_dense_size: dense layer size right before the splex reps
+        """
         self.options = options
         # changeable hyper parameter
         self.drop_out = drop_out
         self.word_vocab_size, self.word_max_len = word_vocab_size, word_max_len
         self.char_vocab_size, self.char_max_len = char_vocab_size, char_max_len
         # hyper parameters that is mostly fixed
-        self.filter, self.dense_size, self.embed_dim = filter, dense_size, embed_dim
+        self.filter, self.dense_size, self.embed_dim, self.kernel_range = filter, dense_size, embed_dim, kernel_range
         # context dimension
         self.context_dim, self.context_dense_size = context_dim, context_dense_size
         self.splex_dense_size = splex_dense_size
@@ -70,11 +104,11 @@ class NN_architecture:
                 if option == 'char':
                     input_content, content_rep = content2rep(option,
                                                              self.char_vocab_size, self.char_max_len, self.drop_out,
-                                                             self.filter, self.dense_size, self.embed_dim)
+                                                             self.filter, self.dense_size, self.embed_dim, self.kernel_range)
                 else:
                     input_content, content_rep = content2rep(option,
                                                              self.word_vocab_size, self.word_max_len, self.drop_out,
-                                                             self.filter, self.dense_size, self.embed_dim)
+                                                             self.filter, self.dense_size, self.embed_dim, self.kernel_range)
                 inputs.append(input_content)
                 last_tensors.append(content_rep)
                 self.load_pretrained_weights(input_content, content_rep,
@@ -99,6 +133,7 @@ class NN_architecture:
 
         self.out_layer = Dense(3, activation='softmax',
                                name='classification')
+
         out = self.out_layer(concatenated_rep)
         self.model = Model(inputs=inputs, outputs=out)
         self.model.summary()
--- src/nn_experiment.py
+++ src/nn_experiment.py
@@ -7,6 +7,9 @@ import subprocess
 from sklearn.metrics import precision_recall_fscore_support
 from keras import backend as K
 
+# calculating the classweight given the y_train
+# the weight will be inversely proprotional to the label
+# belonging to that class
 def calculate_class_weight(y_train):
     class_count = np.sum(y_train, axis=0)
     class_count = 1 / class_count
@@ -14,9 +17,83 @@ def calculate_class_weight(y_train):
     class_weight = dict([(idx, class_count[idx]) for idx in range(len(class_count))])
     return class_weight
 
+# for supervised learning without any pre-training
+# we need to adjust the vocabulary size for all the content input
+# mapping every word that occurs less than twice in the training set to 1
+def adapt_vocab(X_train, X_list):
+    for key in X_train:
+        if key in [option + '_content_input' for option in ['char', 'word']]:
+            wc = {}
+            for xs in X_train[key]:
+                for x in xs:
+                    if wc.get(x) is None:
+                        wc[x] = 0
+                    wc[x] += 1
+            def f(x):
+                return x if (wc.get(x) is not None and wc[x] >= 2) else 1
+            X_train[key] = np.array([[f(x) for x in xs] for xs in X_train[key]])
+            for X in X_list:
+                X[key] = np.array([[f(x) for x in xs] for xs in X[key]])
+
+# an experiment class that runs cross validation
 class Experiment:
 
-    def __init__(self, tweet2data, experiment_dir, comments='', epochs=20, patience=4, **kwargs):
+    def __init__(self, tweet2data, experiment_dir, adapt_train_vocab=False,
+                 comments='', epochs=20, patience=4, **kwargs):
+        """
+        an experiment class that runs cross validation
+        designed to enable easy experiments with combinations of:
+        1) context representation:
+            handled by tweet2data, implement the value for key "context_input"
+            include "context" in options
+        2) pre-training methods:
+            handled by pretrained_weight_dir in the kwargs argument
+            None if there is no pretraining weight available
+        3) char vs. word:
+            specified in "options"
+            options = ['char', 'word'] if you want to include both
+            implement the value for key "word_content_input"
+        options = ['char', 'word', 'context'] if you want to include everything
+
+        Parameters
+        ----------
+        tweet2data: a function that maps a tweet dictionary to a dictionary that contains (key, val) pairs s.t.
+                    key is recognizable by the neural network model,
+                    val is a numpy array
+                    see example in generator_util.py, simplest_tweet2data
+        experiment_dir: the directory that the experiment weights and results will be saved
+        adapt_train_vocab: under supervised training without pretraining,
+                            some vocab will not be seen (twice) in the training set.
+                            if set to True, then vocab occuring less than twice will be removed.
+        comments: the comments that will be written to the README
+        epochs: number of epochs of training during cross validation
+        patience: number of epochs allowable for not having any improvement on the validation set
+        kwargs: arguments that will be passed to initializing the neural network model (shown below)
+
+        ========== below is the parameters needed by the neural network model ==========
+
+        options: an array containing all the options considered in the neural network model ['char', 'word', 'context']
+                    (probably splex in the future)
+                    for each option, the input is mapped to a lower dimension,
+                    then the lower dimension representation of each option is concatenated
+                    and is followed by the final classification layer
+        word_vocab_size: number of word level vocabs to be considered
+        word_max_len: number of words in a tweet sentence
+        char_vocab_size: number of char level vocabs to be considered
+        char_max_len: number of chars in a tweet sentence
+        drop_out: dropout rate for regularization
+        filter: number of filters for each kernel size
+        dense_size: the size of the dense layer following the max pooling layer
+        embed_dim: embedding dimension for character and word level
+        kernel_range: range of kernel sizes
+        pretrained_weight_dir: a dictionary containing the pretrained weight.
+                    e.g. {'char': '../weights/char_ds.weights'} means that the pretrained weight for character level model
+                    is in ../weights/char_ds.weights
+        weight_in_keras: whether the weight is in Keras
+        context_dim: the dimension of context representation
+        context_dense_size: the dense layer size right before the context representation
+        splex_dense_size: dense layer size right before the splex reps
+        """
         # creating the experiment dir
         # automatically generate a README
         if experiment_dir[:-1] != '/':
@@ -25,6 +102,7 @@ class Experiment:
         self.experiment_dir, self.kwargs = experiment_dir, kwargs
         subprocess.call(['rm', '-rf', experiment_dir])
         subprocess.call(['mkdir', experiment_dir])
+        self.adapt_train_vocab = adapt_train_vocab
         with open(self.experiment_dir + 'README', 'w') as readme:
             readme.write(comments + '\n')
             for key in kwargs:
@@ -40,10 +118,15 @@ class Experiment:
         results = []
 
         for fold_idx in range(self.fold):
+            print('cross validation fold %d.' % fold_idx)
+
             # retriving cross validataion data
             fold_data = self.dl.cv_data(fold_idx)
             ((X_train, y_train), (X_val, y_val), (X_test, y_test)) = \
                 create_clf_data(self.tweet2data, fold_data, return_generators=False)
+            if self.adapt_train_vocab:
+                adapt_vocab(X_train, (X_val, X_test))
+
             class_weight = calculate_class_weight(y_train)
 
             # initializing model, train and predict
@@ -81,5 +164,6 @@ class Experiment:
 
 if __name__ == '__main__':
     options = ['word']
-    experiment = Experiment(tweet2data=simplest_tweet2data, experiment_dir='test', options=options)
+    experiment = Experiment(tweet2data=simplest_tweet2data, experiment_dir='test', adapt_train_vocab=True,
+                            options=options)
     experiment.cv()
