357177e61d402f83d9a3d7ba09fc45005e1a89a6
Fei-Tzin Lee <fl2301@columbia.edu>
2018-05-08 02:27:56 +0000

    integrated train_clf + test_clf with data_loader

--- src/CFP/test_clf.py
+++ src/CFP/test_clf.py
@@ -2,22 +2,27 @@ import argparse
 import datetime
 from sklearn.metrics import classification_report
 from cnn_lm_nce import CNN_Model
-from data_utils.TweetReader2 import TweetCorpus
 import numpy as np
 import tensorflow as tf
 from train_clf import batch_iter
 from train_clf import dev_step
+import os
+
+from data_loader import Data_loader
+from collections import namedtuple
+from train_cnn_lm_nce import get_dense_embeddings
 
 def test_clf(sess, model, args, corpus):
-    _, _, X_test, _, _, y_test = corpus.get_data_for_classification()
+    X_test = corpus.X_test
+    y_test = corpus.y_test
 
     saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES), max_to_keep = 1)
 
-    print 'Initializing all variables . . .'
+    print('Initializing all variables . . .')
     sess.run(tf.global_variables_initializer())
 
     if args['pretrained_weights'] is not None:
-        print 'Restoring weights from existing checkpoint . . .'
+        print('Restoring weights from existing checkpoint . . .')
         saver.restore(sess, args['pretrained_weights'])
 
         test_batches = batch_iter(list(zip(X_test, y_test)), args['batch_size'])
@@ -27,42 +32,95 @@ def test_clf(sess, model, args, corpus):
             _, probabilities = dev_step(sess, model, x_batch, y_batch)
             test_probabilities.extend(probabilities)
 
-    print classification_report(np.argmax(y_test, axis = 1), np.argmax(test_probabilities, axis = 1), target_names = corpus.get_class_names())
+    print(classification_report(y_test, np.argmax(test_probabilities, axis = 1), target_names = corpus.class_names))
 
 def main(args):
 
-    corpus = TweetCorpus(test_file = args['test_file'], dictionaries_file = args['dictionaries_file'])
+    #corpus = TweetCorpus(test_file = args['test_file'], dictionaries_file = args['dictionaries_file'])
 
     ts = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     args['ts'] = ts
 
-    class_weights = [corpus.class_weights[i] for i in range(len(corpus.label2idx))]
+    #class_weights = [corpus.class_weights[i] for i in range(len(corpus.label2idx))]
+
+    dl = Data_loader()
+    label2idx = dl.get_label2idx()
+    idx2label = {v:k for k, v in label2idx.items()}
+    class_weights_dict = dl.get_class_weights()
+    class_weights = [class_weights_dict[idx2label[i]] for i in range(len(label2idx))]
+    counts = dl.get_freq_counts()
+    W = get_dense_embeddings(dl.token2id, args['emb_dim'], emb_file = args['emb_file'])
+
+    #X_unld_tr, X_unld_val = dl.unlabeled_tr_val_as_arrays(min_len = 5, padded = True)
+
+    #corpus = TweetCorpus(train_file = args['train_file'], val_file = args['val_file'], test_file = args['test_file'] , dictionaries_file = args['dictionaries_file'])
+
+    Corpus = namedtuple('Corpus', 'label2idx idx2label token2idx idx2token counts class_weights W X_test y_test pad_token_idx class_names')
+
+    ts = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    args['ts'] = ts
 
     # print_hyper_params(args)
 
-    with tf.Graph().as_default():
-        session_conf = tf.ConfigProto(
-          allow_soft_placement = True,
-          log_device_placement = False)
-        sess = tf.Session(config = session_conf)
-        with sess.as_default():
-            cnn = CNN_Model(sequence_length = corpus.max_len,
-                               num_classes = len(corpus.label2idx),
-                               vocab_size = len(corpus.token2idx),
-                               embedding_size = len(corpus.W[0]),
-                               filter_sizes = [1, 2, 3, 4, 5],
-                               num_filters = args['nfeature_maps'],
-                               embeddings = corpus.W,
-                               class_weights = class_weights)
+    pw = None
 
-            # sess, model, args, corpus
-            test_clf(sess, cnn, args, corpus)
+    if args['pretrained_weights'] is not None:
+        pw = args['pretrained_weights']
+
+    # print_hyper_params(args)
+
+    for fold_idx in range(5):
+        _, _, _, _, X_test, y_test = dl.cv_data_as_arrays(fold_idx=fold_idx, min_len=5, padded=True)
+
+        y_test = np.array([label2idx[y] for y in y_test])
+
+        corpus = Corpus(label2idx = label2idx,
+                        idx2label = idx2label,
+                        token2idx = dl.token2id,
+                        idx2token = dl.id2token,
+                        counts = counts,
+                        class_weights = class_weights,
+                        W = W,
+                        X_test = X_test,
+                        y_test = y_test,
+                        pad_token_idx = 0,
+                        class_names = [idx2label[0], idx2label[1], idx2label[2]]
+                        )
+
+        if pw is not None:
+            suffix = 'model-29892'
+            prefix = os.path.join(pw, 'clf_runs_' + str(fold_idx), 'checkpoints')
+
+            for f in os.listdir(prefix):
+                if f.startswith('model-'):
+                    suffix = f.split('.')[0]
+                    break
+            
+            args['pretrained_weights'] = os.path.join(prefix, suffix)
+        
+        with tf.Graph().as_default():
+            session_conf = tf.ConfigProto(
+                allow_soft_placement = True,
+                log_device_placement = False)
+            sess = tf.Session(config = session_conf)
+            with sess.as_default():
+                cnn = CNN_Model(sequence_length = dl.max_len,
+                                num_classes = len(label2idx),
+                                vocab_size = dl.vocab_size,
+                                embedding_size = len(corpus.W[0]),
+                                filter_sizes = [1, 2, 3, 4, 5],
+                                num_filters = args['nfeature_maps'],
+                                embeddings = corpus.W,
+                                class_weights = class_weights)
+                
+                # sess, model, args, corpus
+                test_clf(sess, cnn, args, corpus)
 
 def parse_arguments():
 
     parser = argparse.ArgumentParser(description = '')
-    parser.add_argument('-tst', '--test_file', type = str, help = 'labeled test file')
-    parser.add_argument('-dict', '--dictionaries_file', type = str, help = 'pickled dictionary file')
+    parser.add_argument('-ef', '--emb_file', type = str, default = None, help = '.bin file containing pre-trained embeddings')
+    parser.add_argument('-ed', '--emb-dim', type = int, default = 300, help = 'If emb-file is given then the value passed here will be ignored.')
     parser.add_argument('-w', '--pretrained_weights', type = str, default = None, help = 'Path to pretrained weights file')
     parser.add_argument('-nfmaps', '--nfeature_maps', type = int, default = 200)
     parser.add_argument('-bsz', '--batch_size', type = int, default = 256)
--- src/CFP/train_clf.py
+++ src/CFP/train_clf.py
@@ -9,9 +9,12 @@ import time
 
 from Nadam import NadamOptimizer
 from cnn_lm_nce import CNN_Model
-from data_utils.TweetReader2 import TweetCorpus
 import numpy as np
 import tensorflow as tf
+from train_cnn_lm_nce import get_dense_embeddings
+from collections import namedtuple
+
+from data_loader import Data_loader
 
 def batch_iter(data, batch_size, shuffle = False):
     """
@@ -65,9 +68,10 @@ def train_clf(sess, model, args, corpus):
     grad_summaries_merged = tf.summary.merge(grad_summaries)
 
     # Output directory for models and summaries
-    if os.path.isdir(os.path.join(args['model_save_dir'], "clf_runs")):
-        shutil.rmtree(os.path.join(args['model_save_dir'], "clf_runs"))
-    out_dir = os.path.abspath(os.path.join(args['model_save_dir'], "clf_runs"))
+    clf_dir = 'clf_runs_' + str(args['fold_idx'])
+    if os.path.isdir(os.path.join(args['model_save_dir'], clf_dir)):
+        shutil.rmtree(os.path.join(args['model_save_dir'], clf_dir))
+    out_dir = os.path.abspath(os.path.join(args['model_save_dir'], clf_dir))
     print("Writing to {}\n".format(out_dir))
 
     # Summaries for loss and accuracy
@@ -91,11 +95,11 @@ def train_clf(sess, model, args, corpus):
 
     saver = tf.train.Saver(tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES), max_to_keep = 1)
 
-    print 'Initializing all variables . . .'
+    print('Initializing all variables . . .')
     sess.run(tf.global_variables_initializer())
 
     if args['pretrained_weights'] is not None:
-        print 'Restoring weights from existing checkpoint . . .'
+        print('Restoring weights from existing checkpoint . . .')
         saver.restore(sess, args['pretrained_weights'])
 
     def train_step(x_batch, y_batch):
@@ -115,16 +119,21 @@ def train_clf(sess, model, args, corpus):
 
         return loss, probabilities
 
-    X_tr, X_val, X_test, y_tr, y_val, y_test = corpus.get_data_for_classification()
+    X_tr = corpus.X_tr
+    X_val = corpus.X_val
+    X_test = corpus.X_test
+    y_tr = corpus.y_tr
+    y_val = corpus.y_val
+    y_test = corpus.y_test
 
     best_val_f = 0
     best_val_probabilities = None
     patience = 5
 
-    for epoch in xrange(args['n_epochs']):
+    for epoch in range(args['n_epochs']):
         _tr_loss = 0
         # tr_probabilities = []
-        print 'Epoch %d:' % (epoch)
+        print('Epoch %d:' % (epoch))
         _start = time.time()
         batches = batch_iter(
             list(zip(X_tr, y_tr)), args['batch_size'], shuffle = True)
@@ -137,8 +146,8 @@ def train_clf(sess, model, args, corpus):
 
         current_step = tf.train.global_step(sess, global_step)
         tr_loss = _tr_loss / len(X_tr)
-        print 'Run time: %d s' % (time.time() - _start)
-        print 'Training Loss: %f' % (tr_loss)
+        print('Run time: %d s' % (time.time() - _start))
+        print('Training Loss: %f' % (tr_loss))
 
         val_batches = batch_iter(list(zip(X_val, y_val)), args['batch_size'])
         _val_loss = 0
@@ -150,9 +159,9 @@ def train_clf(sess, model, args, corpus):
             val_probabilities.extend(probabilities)
 
         val_loss = _val_loss / len(X_val)
-        val_f = f1_score(np.argmax(y_val, axis = 1), np.argmax(val_probabilities, axis = 1), average = 'macro')
+        val_f = f1_score(np.argmax([[1, 0, 0] if x == 0 else [0, 0, 1] if x == 2 else [0, 1, 0] for x in y_val], axis = 1), np.argmax(val_probabilities, axis = 1), average = 'macro')
 
-        print 'Val Loss: %f, Val macro f: %f' % (val_loss, val_f)
+        print('Val Loss: %f, Val macro f: %f' % (val_loss, val_f))
 
         if val_f > best_val_f:
             best_val_f = val_f
@@ -165,55 +174,102 @@ def train_clf(sess, model, args, corpus):
             print("\n")
 
         if patience == 0:
-            print 'Early stopping . . .'
+            print('Early stopping . . .')
             break
 
-    print classification_report(np.argmax(y_val, axis = 1), np.argmax(best_val_probabilities, axis = 1), target_names = corpus.get_class_names())
+    print(classification_report(y_val, np.argmax(best_val_probabilities, axis = 1), target_names = corpus.class_names))
 
 def main(args):
 
-    corpus = TweetCorpus(train_file = args['train_file'], val_file = args['val_file'], test_file = args['test_file'] , dictionaries_file = args['dictionaries_file'])
+    dl = Data_loader()
+    label2idx = dl.get_label2idx()
+    idx2label = {v:k for k, v in label2idx.items()}
+    class_weights_dict = dl.get_class_weights()
+    class_weights = [class_weights_dict[idx2label[i]] for i in range(len(label2idx))]
+    counts = dl.get_freq_counts()
+    W = get_dense_embeddings(dl.token2id, args['emb_dim'], emb_file = args['emb_file'])
+
+    #X_unld_tr, X_unld_val = dl.unlabeled_tr_val_as_arrays(min_len = 5, padded = True)
+
+    #corpus = TweetCorpus(train_file = args['train_file'], val_file = args['val_file'], test_file = args['test_file'] , dictionaries_file = args['dictionaries_file'])
+
+    Corpus = namedtuple('Corpus', 'label2idx idx2label token2idx idx2token counts class_weights W X_tr y_tr X_val y_val X_test y_test pad_token_idx class_names')
 
     ts = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     args['ts'] = ts
 
-    class_weights = [corpus.class_weights[i] for i in range(len(corpus.label2idx))]
-
     # print_hyper_params(args)
 
-    with tf.Graph().as_default():
-        session_conf = tf.ConfigProto(
-          allow_soft_placement = True,
-          log_device_placement = False)
-        sess = tf.Session(config = session_conf)
-        with sess.as_default():
-            cnn = CNN_Model(sequence_length = corpus.max_len,
-                               num_classes = len(corpus.label2idx),
-                               vocab_size = len(corpus.token2idx),
-                               embedding_size = len(corpus.W[0]),
-                               filter_sizes = [1, 2, 3, 4, 5],
-                               num_filters = args['nfeature_maps'],
-                               embeddings = corpus.W,
-                               class_weights = class_weights)
+    pw = None
 
-            print 'List of trainable variables:'
-            for i in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
-                print i.name
+    if args['pretrained_weights'] is not None:
+        pw = args['pretrained_weights']
 
-            # sess, model, args, corpus
-            train_clf(sess, cnn, args, corpus)
+    for fold_idx in range(5):
+        X_tr, y_tr, X_val, y_val, X_test, y_test = dl.cv_data_as_arrays(fold_idx=fold_idx, min_len = 5, padded = True)
+
+        y_tr = np.array([label2idx[y] for y in y_tr])
+        y_val = np.array([label2idx[y] for y in y_val])
+        y_test = np.array([label2idx[y] for y in y_test])
+
+        #y_tr = np.array([[1, 0, 0] if 'loss' in x.lower() else [0, 0, 1] if 'aggr' in x.lower() else [0, 1, 0] for x in y_tr])
+        #y_val = np.array([[1, 0, 0] if 'loss' in x.lower() else [0, 0, 1] if 'aggr' in x.lower() else [0, 1, 0] for x in y_val])
+        #y_test = np.array([[1, 0, 0] if 'loss' in x.lower() else [0, 0, 1] if 'aggr' in x.lower() else [0, 1, 0] for x in y_test])
+
+        corpus = Corpus(label2idx = label2idx,
+                        idx2label = idx2label,
+                        token2idx = dl.token2id,
+                        idx2token = dl.id2token,
+                        counts = counts,
+                        class_weights = class_weights,
+                        W = W,
+                        X_tr = X_tr,
+                        y_tr = y_tr,
+                        X_val = X_val,
+                        y_val = y_val,
+                        X_test = X_test,
+                        y_test = y_test,
+                        pad_token_idx = 0,
+                        class_names = [idx2label[0], idx2label[1], idx2label[2]]
+                        )
+
+        if pw is not None:
+            args['pretrained_weights'] = os.path.join(pw, 'lm_runs_' + str(fold_idx), 'checkpoints', 'model-29892')#'model-29892.index')
+
+        args['fold_idx'] = fold_idx
+        print ('Running fold: %d' % (fold_idx))
+        
+        with tf.Graph().as_default():
+            session_conf = tf.ConfigProto(
+                allow_soft_placement = True,
+                log_device_placement = False)
+            sess = tf.Session(config = session_conf)
+            with sess.as_default():
+                cnn = CNN_Model(sequence_length = dl.max_len,
+                                num_classes = len(label2idx),
+                                vocab_size = dl.vocab_size,
+                                embedding_size = len(corpus.W[0]),
+                                filter_sizes = [1, 2, 3, 4, 5],
+                                num_filters = args['nfeature_maps'],
+                                embeddings = corpus.W,
+                                class_weights = class_weights)
+                
+                print('List of trainable variables:')
+                for i in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES):
+                    print(i.name)
+
+                # sess, model, args, corpus
+                train_clf(sess, cnn, args, corpus)
 
 def parse_arguments():
 
     parser = argparse.ArgumentParser(description = '')
     # even though short flags can be used in the command line, they can not be used to access the value of the arguments
     # i.e args['pt'] will give KeyError.
-    parser.add_argument('-tr', '--train_file', type = str, help = 'labeled train file')
-    parser.add_argument('-val', '--val_file', type = str, help = 'labeled validation file')
-    parser.add_argument('-tst', '--test_file', type = str, help = 'labeled test file')
-    parser.add_argument('-dict', '--dictionaries_file', type = str, help = 'pickled dictionary file')
     parser.add_argument('-sdir', '--model_save_dir', type = str, help = 'directory where trained model should be saved')
-    parser.add_argument('-w', '--pretrained_weights', type = str, default = None, help = 'Path to pretrained weights file')
+    parser.add_argument('-w', '--pretrained_weights', type = str, default = None, help = 'Path to pretrained weights directory')
+    parser.add_argument('-ef', '--emb_file', type = str, default = None, help = '.bin file containing pre-trained embeddings')
+    parser.add_argument('-ed', '--emb-dim', type = int, default = 300, help = 'If emb-file is given then the value passed here will be ignored.')
     parser.add_argument('-epochs', '--n_epochs', type = int, default = 30)
     parser.add_argument('-nfmaps', '--nfeature_maps', type = int, default = 200)
     parser.add_argument('-do', '--dropout', type = float, default = 0.5)
