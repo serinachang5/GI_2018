c370906c778414039f354d6d7467b43d113c4a53
Ruiqi <ruiqi.zhong@gatech.edu>
2018-05-13 13:58:51 -0400

    standard experiments

--- src/test_nn_experiments.py
+++ src/test_nn_experiments.py
@@ -1,10 +1,30 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.XIB" version="3.0" toolsVersion="11134" systemVersion="15F34" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" colorMatched="YES">
-    <dependencies>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="11106"/>
-    </dependencies>
-    <objects>
-        <placeholder placeholderIdentifier="IBFilesOwner" id="-1" userLabel="File's Owner"/>
-        <placeholder placeholderIdentifier="IBFirstResponder" id="-2" customClass="UIResponder"/>
-    </objects>
-</document>
+from nn_experiment import Experiment
+import numpy as np
+
+if __name__ == '__main__':
+    # plain words CNN, without pretraining
+    options = ['word']
+    experiment = Experiment(experiment_dir='test_word', input_name2id2np=None, adapt_train_vocab=True,
+                            options=options)
+    experiment.cv()
+
+    # plain char level CNN, without pretraining
+    options = ['char']
+    experiment = Experiment(experiment_dir='test_char', input_name2id2np=None, adapt_train_vocab=True,
+                            options=options)
+    experiment.cv()
+
+    # with word2vec embeddings
+    pretrained_weight_dirs = ({'aggression_word_embed': ['../weights/w2v_word_s300_w5_mc5_ep20.np'],
+                              'loss_word_embed': ['../weights/w2v_word_s300_w5_mc5_ep20.np']})
+    experiment = Experiment(experiment_dir='test_word2vec_' + str(_), input_name2id2np=None,
+                            pretrained_weight_dirs=pretrained_weight_dirs, options=options)
+    experiment.cv()
+
+    # combining word, char and other inputs
+    labeled_tids = np.loadtxt('../data/labeled_tids.np', dtype='int')
+    input_name2id2np = {'dummpy': dict([(tid, np.random.normal(shape=(300,)))
+                                        for tid in labeled_tids])}
+    experiment = Experiment(experiment_dir='test_combine_',
+                            input_name2id2np=input_name2id2np, adapt_train_vocab=True,
+                            options=options)
