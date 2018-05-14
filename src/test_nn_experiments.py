770194927c7761f2a9a4bb201616a5ea0e3139ad
Ruiqi <ruiqi.zhong@gatech.edu>
2018-05-13 23:30:20 -0400

    documentation of word embedding weight loading

--- src/test_nn_experiments.py
+++ src/test_nn_experiments.py
@@ -15,7 +15,27 @@ if __name__ == '__main__':
     experiment.cv()
 
     # with word2vec embeddings
+    '''
+        with word2vec embeddings
+        pretrained_weight_dirs is a dictionary, where each key is the layer name
+        and the value is a list of weight directories in numpy file format
+        
+        Currently, since the program trains models for both aggression and loss class
+        Except the input layer, all the other layers will now have a "class" prefix
+        Therefore, if one weight can be used by two models, you need to initialize both of them
+        
+        About the keys: each of them should match a layer name in the model
+        After a weight of a layer is loaded, the program will print a message that weight loaded successfully
+        (after model.summary() shows the architecture)
+        
+        About the weights:
+        Each of them is an array of numpy directories that can be called by np.loadtxt
+        The list of numpy array should be exactlyt the same as layer.get_weights
+        (see keras documetentation for details)
+        In particular, word embedding layer has weight: [<a matrix of size vocab * embed_dim>]
+    '''
     options = ['word']
+    # note that each of the key has appeared as a layer name in the model
     pretrained_weight_dirs = ({'aggression_word_embed': ['../weights/w2v_word_s300_w5_mc5_ep20.np'],
                               'loss_word_embed': ['../weights/w2v_word_s300_w5_mc5_ep20.np']})
     experiment = Experiment(experiment_dir='test_word2vec_' + str(_), input_name2id2np=None,
